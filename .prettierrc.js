8864e06a5cbc809bfd10f92765da4e3794fb64cf
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-08-31 15:20:05 +0900

    chore: merge lint & prettier rules from upstream

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,6 @@
 module.exports = {
-  parser: 'typescript',
-  useTabs: false,
-  tabWidth: 2,
-  printWidth: 100,
-  semi: false,
-  singleQuote: true,
   trailingComma: 'all',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
+  semi: true,
+  singleQuote: true,
   arrowParens: 'always',
-  rangeStart: 0,
-  // rangeEnd: Infinity,
-  proseWrap: 'never', // <always|never|preserve>
 }
